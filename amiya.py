98288a640787173a4ab39183d44ad3a0854ec372
Despit <47079628+Despit@users.noreply.github.com>
2023-05-17 16:34:26 +0800

    1.01
    修复网站更新后只下载最新漫画章节的问题

--- amiya.py
+++ amiya.py
@@ -1,13 +1,13 @@
-'''
-Author: Azulene
-Date: 2022-10-20 18:55:54
-LastEditors: Azulene
-LastEditTime: 2022-10-22 13:52:17
-FilePath: /hy_maga_python/main.py
-Description: :-P
+# '''
+# Author: Azulene
+# Date: 2022-10-20 18:55:54
+# LastEditors: Azulene
+# LastEditTime: 2022-10-24 13:00:39
+# FilePath: \undefinede:\amiya\amiya.py
+# Description: :-P
 
-Copyright (c) 2022 by Azulene, All Rights Reserved. 
-'''
+# Copyright (c) 2022 by Azulene, All Rights Reserved. 
+# '''
 import requests
 import json
 import re
@@ -16,7 +16,7 @@ import os
 #from bs4 import BeautifulSoup
 from urllib import request
 
-headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0"}
+headers={"Connection":"close","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0"}
 local="temp"
 
 def MakeDir(Name_comic,Name_Subtitle,Name_title):
@@ -35,21 +35,22 @@ def CallBack(cur_down, cur_size, total_size):
 pass
 
 def Download(Id_comic,Id_episode,Name_comic,Name_Subtitle,Name_title):
-    global page
-    global local
+    global page,local,count
     MakeDir(Name_comic,Name_Subtitle,Name_title)
     url_comic="https://terra-historicus.hypergryph.com/api/comic/"+Id_comic+"/episode/"+Id_episode+"/page?pageNum=%d"
-    url_page=requests.get(url_comic%page).json()
-    while(url_page["msg"]!="NO_RESOURCE"):
+    url_page=requests.get(url_comic%page)
+    while(url_page.status_code == 200):
+        url_page=url_page.json()
         url_pic=url_page["data"]["url"]
-        PathName="./"+Name_comic+"/"+Name_Subtitle+" "+Name_title+"/img"+"%d"%page+".jpg"
+        PathName="./"+Name_comic+"/"+Name_Subtitle+" "+Name_title+"/img"+"%d"%count+".jpg"
         local=PathName
         if not os.path.isfile(PathName):
             request.urlretrieve(url=url_pic,filename=PathName,reporthook=CallBack)
         else:
             print("%s 已存在"%PathName)
         page+=1
-        url_page=requests.get(url_comic%page).json()
+        count+=1
+        url_page=requests.get(url_comic%page)
 pass
 #https://terra-historicus.hypergryph.com/comic/9382/episode/3195
 if __name__ == "__main__":
@@ -59,8 +60,10 @@ if __name__ == "__main__":
     info=requests.get(url="https://terra-historicus.hypergryph.com/api/comic/"+Id_comic,headers=headers)
     FileName_comic=info.json()["data"]["title"]
     Index_episode=info.json()["data"]["episodes"]
+    count=1
     for Seq_episode in range(len(Index_episode)):
         page=1
-        Download(Id_comic,Index_episode[Seq_episode]["cid"],FileName_comic,Index_episode[Seq_episode]["shortTitle"],Index_episode[Seq_episode]["title"])
+        count=1
+        Download(Id_comic,Index_episode[len(Index_episode)-Seq_episode-1]["cid"],FileName_comic,Index_episode[len(Index_episode)-Seq_episode-1]["shortTitle"],Index_episode[len(Index_episode)-Seq_episode-1]["title"])
     print("下载完成")
 pass 
